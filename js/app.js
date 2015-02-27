cbf695067557c2f789cd49593e1e00a278b0086d
Ryan Gladstone <ryan.gladstone@washpost.com>
2015-02-26 20:52:34 -0500

    Implemented Material Design to improve the UI. Added comments.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 
 // App definition + dependencies
 var myApp = angular.module('myApp', [
+    // necessary for matching the URL to an available resource
     'ngRoute',
-    'ngAnimate',
-    'mgcrea.ngStrap'
+    // Material Design by Google
+    'ngMaterial'
     ]);
 
 // Route definition 
@@ -12,55 +13,69 @@ myApp.config(['$routeProvider',
     function ($routeProvider) {
         
         $routeProvider
+            // Homepage includes Expunge D.C. overview and link to the wizard
             .when('/', {
                 templateUrl: 'views/home.html',
                 controller: 'homeController'
             })
+            // Information on an misdemeanor or felony being ineligible for expungement, either temporarily or permanently 
             .when('/eligibility-check/ineligible/:reason?', {
                 templateUrl: 'views/eligibility-check-ineligible.html',
                 controller: 'eligibilityIneligibleController'
             })
+            // Information on an misdemeanor or felony being eligible for expungement, including next steps
             .when('/eligibility-check/eligible', {
                 templateUrl: 'views/eligibility-check-eligible.html',
                 controller: 'eligibilityEligibleController'
             })
+            // The wizard!
             .when('/eligibility-check', {
                 templateUrl: 'views/eligibility-check.html',
                 controller: 'eligibilityController'
             })
+            // If the user attempts to visit a route that doesn't match any of the patterns above, re-direct them to the homepage
             .otherwise({
                 redirectTo: '/'
-			      });
+            });
 }]);
 
 
+// Homepage controller
 myApp.controller('homeController',
-    ['$scope', '$routeParams',
-        function ($scope, $routeParams) {
+    ['$scope', 
+        function ($scope) {
             
+
         }
 ]);
 
+// Eligibility Checker Controller -- AKA The Wizard
 myApp.controller('eligibilityController',
-    ['$scope', '$http', '$routeParams', '$location',
-        function ($scope, $http, $routeParams, $location) {
+    ['$scope', '$http', '$location',
+        function ($scope, $http, $location) {
         
+        // step_in_process controls which question the user sees
+        // default to the first step, 0
         $scope.step_in_process = 0;
 
+        // Grab the ineligible misdemeanors from a static JSON file stored at the root of the project
         $http.get('ineligible-misdemeanors.json')
         .success(function(data, status, headers, config) {
+            // if the app successfully gets misdemeanor data from the JSON file, assign it to $scope.ineligibleMisdemeanors for use in the wizard
             $scope.ineligibleMisdemeanors  = data;
         });
         
+        // This function is used to advance the user through the wizard
         $scope.getToStep = function (step) {
-          console.log('Going to step: ', step);
           $scope.step_in_process = step;
         }
          
+        // This function redirects the user to the ineligible page
         $scope.ineligible = function(reason) {
           $location.path('eligibility-check/ineligible/reason');
         };
         
+        // This function redirects the user to the eligible page
         $scope.eligible = function() {
           $location.path('eligibility-check/eligible');
         }
@@ -68,18 +83,34 @@ myApp.controller('eligibilityController',
         }
 ]);
 
+// This controller controls the ineligible page. 
+// There is currently just static content on this page
 myApp.controller('eligibilityIneligibleController',
-    ['$scope', '$routeParams',
-        function ($scope, $routeParams) {
+    ['$scope', 
+        function ($scope) {
         
            
         }
 ]);
 
+// This controller controls the eligible page. 
+// There is currently just static content on this page
 myApp.controller('eligibilityEligibleController',
-    ['$scope', '$routeParams',
-        function ($scope, $routeParams) {
+    ['$scope', 
+        function ($scope) {
         
+           
+        }
+]);
+
+
+// Partial 
+myApp.controller('titlebarController',
+    ['$scope', '$mdSidenav',
+        function ($scope, $mdSidenav) {
+            $scope.openLeftMenu = function() {
+                $mdSidenav('left').toggle();
+            };
            
         }
 ]);
